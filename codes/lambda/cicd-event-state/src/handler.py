076108d69317aad93d11b2fdc3cc0e35423d9bba
mhs <mhs@modular.cloud>
2023-02-13 09:47:35 -0800

    Revmoe unused things

--- codes/lambda/cicd-event-state/src/handler.py
+++
@@ -1,9 +0,0 @@
-
-def handle(event, context):
-    print('event--->', event)
-    
-    print('stage', event['detail']['stage'])
-    print('action', event['detail']['action'])
-    print('state', event['detail']['state'])
-    
-    return 'ok'
